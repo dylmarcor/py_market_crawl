61efcd574df578554a6309a50ef52cb008fa17eb
dylmarcor <dylancorbett@icloud.com>
2026-03-01 15:35:31 -0500

    added local DB for testing

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,3 +5,8 @@ router = APIRouter()
 @router.get("/health")
 def health_check():
     return {"status": "ok"}
+
+# Add logic for POST/GET for symbols
+@router.get("/symbols")
+def symbol_check():
+    return {"status": "ok"}
--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/marketdata"
+DATABASE_URL =  os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/marketdata"
+)
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
